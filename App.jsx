3f486779f3c36e1eff3e80704592f5d7e25bdccc
nikhil9845 <120568081+nikhil9845@users.noreply.github.com>
2023-03-07 07:56:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+// import Nav from "./component/Nav"
+// import "./global.css"
+
+// const App=()=>{
+//     return(
+//         <Nav/>
+//     )
+// }
+// export default App;
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {createRoot} from "react-dom/client"
+import Netflix from "./component/Netflix";
+// import App from "./App";
+// createRoot(document.getElementById("root")).render(<App/>)
+
+createRoot(document.getElementById("root")).render(<Netflix/>)
